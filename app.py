21c25f0a6e935689128d72987c7b6dcec44d97e6
Mama mia <bee@baa.s>
2023-05-16 01:23:04 +0400

    formatting

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def add_group():
 @app.route('/')
 def index():
     groups = db.groups.find()
-	return render_template('index.html', groups=groups)
+    return render_template('index.html', groups=groups)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
