4370e11dd6a0e25144323eb16c677a9438d243a1
Mama mia <bee@baa.s>
2023-05-16 01:14:08 +0400

    update html

--- app.py
+++ app.py
@@ -2,26 +2,40 @@ from flask import Flask, render_template, request, redirect, url_for
 import pymongo
 import csv
 import pprint
+import json
 
 app = Flask(__name__)
-db = pymongo.MongoClient('mongodb://mongo:27017/', username='root', password='root')
-
+client = pymongo.MongoClient('mongodb://mongo:27017/', username='root', password='root')
+db = client["groupspam"]
 
 @app.route('/upload_userlist', methods=['POST'])
 def upload_userlist():
-    #form with file upload
-    #get file, parse it and upload to mongo
     userlist = []
     file = request.files['file']
     if file:
         string = file.read().decode('utf-8')
         lines = string.split('\n')
-        return pprint.pformat(lines)
+        lines = [line.replace('\r', '') for line in lines]
+        db.insert_one({ 'name' : file.name, 'users': lines})
+        return 200, file.name
+    return 500, 'Error uploading file'
+
+@app.route('/get_groups', methods=['GET'])
+def get_groups():
+    groups = []
+    for group in db.groups.find():
+        groups.append(group)
+    return json.dumps(groups)
+
+@app.route('/edit_group', methods=['POST'])
+def edit_group():
+
 
 
 @app.route('/')
 def index():
-	return render_template('index.html', groups=['Group 1', 'Group 2', 'Group 3', 'Group 4'])
+    groups = db.groups.find()
+	return render_template('index.html', groups=groups)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, debug=True)
